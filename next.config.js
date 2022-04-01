7ce137b4092b03681ca37e3b7eaee388d8afd048
Goku <97279666+tigerhole@users.noreply.github.com>
2022-04-01 10:20:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 // const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
 
-//const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//  enabled: process.env.ANALYZE === 'true'
-//})
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
-//module.exports = withBundleAnalyzer({
-//  images: {
-//    domains: ['pbs.twimg.com']
-//  }
-//})
-
-const withOptimizedImages = require('next-optimized-images');
-
-module.exports = withOptimizedImages({
-  /* config for next-optimized-images */
+module.exports = withBundleAnalyzer({
   images: {
     domains: ['pbs.twimg.com']
   }
+})
+
+//const withOptimizedImages = require('next-optimized-images');
+
+//module.exports = withOptimizedImages({
+  /* config for next-optimized-images */
+  //images: {
+    //domains: ['pbs.twimg.com']
+ // }
   // your config for other plugins or the general next.js here...
-});
+//});
